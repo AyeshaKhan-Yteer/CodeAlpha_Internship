366c44c766ead599925a9adb3f574a6da3e29d37
Ayesha Khan <144998423+AyeshaKhan-Yteer@users.noreply.github.com>
2024-05-25 19:20:29 +0500

    Add files via upload

---
+++ CodeAlpha_MusicPlayer/data.js
@@ -0,0 +1,50 @@
+let songs = [
+  {
+    name: "Dj Snake - Let Me Love You",
+    path: "assets/musics/DJ-Snake---Let-Me-Love-You.mp3",
+    artist: "Justin Bieber x Dj Snake",
+    cover: "assets/images/IMG2.jpg",
+  },
+  {
+    name: "Future - Mask_Off",
+    path: "assets/musics/Future-Mask_Off(Remix).mp3",
+    artist: "Future",
+    cover: "assets/images/IMG5.jpg",
+  },
+  {
+    name: "Halsey - Without Me",
+    path: "assets/musics/Halsey-Without_Me.mp3",
+    artist: "Halsey",
+    cover: "assets/images/IMG3.jpg",
+  },
+  {
+    name: "Jhope- Arson",
+    path: "assets/musics/j hope   Arson.mp3",
+    artist: "JHope BTS",
+    cover: "assets/images/IMG8.jpg",
+  },
+  {
+    name: "Isabel - Favorite",
+    path: "assets/musics/Isabel LaRosa-favorite.mp3",
+    artist: "Isabel LaRosa",
+    cover: "assets/images/IMG4.jpg",
+  },
+  {
+    name: "Camila - Never Be The Same",
+    path: "assets/musics/neverbethesame.mp3",
+    artist: "Camila Cabello",
+    cover: "assets/images/IMG1.jpg",
+  },
+  {
+    name: "The Kid LAROI- STAY",
+    path: "assets/musics/Stay.mp3",
+    artist: "Justin Bieber x The Kid LAROI",
+    cover: "assets/images/IMG6.jpg",
+  },
+  {
+    name: "Selena Gomez- Wolves",
+    path: "assets/musics/Wolves.mp3",
+    artist: "Selena Gomez x Marshmello",
+    cover: "assets/images/IMG7.jpg",
+  },
+];
